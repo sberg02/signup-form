f589b6ffd0838776c5f76f8fd350158221317f84
sberg02 <114252187+sberg02@users.noreply.github.com>
2024-01-08 19:35:15 -0600

    finalized html and css

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-//commenting out javascript for now. will revisit.
-
-/*const btnSelect = document.querySelector('.pass');
-btnSelect.addEventListener('keydown', pwCheck);
+const btnSelect = document.querySelector('#submit');
+btnSelect.addEventListener('click', pwCheck);
 
 function pwCheck () {
-    let inputSelect = document.querySelector('.pass');
+    let inputSelect = document.getElementById('pw');
+    let inputSelect2 = document.getElementById('conf-pw');
     let pass1 = document.getElementById('pw').value;
     let pass2 = document.getElementById('conf-pw').value;
+    let message = document.querySelector('#message');
     if (pass1 != pass2) {
-        inputSelect.classList.add('.error');
+        inputSelect.classList.add('error');
+        inputSelect2.classList.add('error');
+        message.style.visibility('visible');
     }
     else {
         return;
     }
-};*/
+};
 
